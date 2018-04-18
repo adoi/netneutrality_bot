aa38aacaa778e022b797eaeded2f5e06a312df07
adoi <adonismurati@gmail.com>
2018-04-18 21:20:05 +0200

    Refactoring

--- bot.js
+++ bot.js
@@ -1,84 +1,91 @@
-console.log('Initial start');
+console.log("Initial start");
 
-const Twit = require('twit');
-const config = require('./config');
+const Twit = require("twit");
+const config = require("./config");
 
 const T = new Twit(config);
 
 const firstTweet = {
-    status: '#NetNeutrality from the bot'
-}
+  status: "#NetNeutrality from the bot"
+};
 
-T.post('statuses/update', firstTweet, function(err, data, response){
-    if(err){
-        console.log('Error Ocurred');
-    }else{
-        console.log('Status posted successfully.');
-    }
+T.post("statuses/update", firstTweet, (err, data, response) => {
+  if (err) {
+    console.log("Error Ocurred");
+  } else {
+    console.log("Status posted successfully.");
+  }
 });
 
 // The 'favorite' feature
-const favoriteTweet = function(){
-    const params = {
-        q: '#NetNeutrality',
-        result_type: 'recent',
-        lang: 'en'
-    }
+const favoriteTweet = function() {
+  const params = {
+    q: "#NetNeutrality",
+    result_type: "recent",
+    lang: "en"
+  };
 
-    T.get('search/tweets', params, function(err, data){
-        let foundTweet = data.statuses;
-        let randomTweet = randomTw(foundTweet);
+  T.get("search/tweets", params, (err, data) => {
+    let foundTweet = data.statuses;
+    let randomTweet = randomTw(foundTweet);
 
-        if(typeof randomTweet != 'undefined'){
-            T.post('favorites/create', {id: randomTweet.id_str}, function(err, response){
-                if(err){
-                    console.log('Error Ocurred. Cannot favorite tweet');
-                }else{
-                    console.log('Favorited from the bot.');
-                }
-            });
+    if (typeof randomTweet != "undefined") {
+      T.post(
+        "favorites/create",
+        { id: randomTweet.id_str },
+        (err, response) => {
+          if (err) {
+            console.log("Error Ocurred. Cannot favorite tweet");
+          } else {
+            console.log("Favorited from the bot.");
+          }
         }
-    });
-}
+      );
+    }
+  });
+};
 
-function randomTw(arr){
-    let index = Math.floor(Math.random()*arr.length);
-    return arr[index];
+function randomTw(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
 }
 
 favoriteTweet();
 
-setInterval(favoriteTweet, 10000*6*10);
-
+setInterval(favoriteTweet, 10000 * 6 * 10);
 
 //The 'Reply' feature
-const stream = T.stream('user');
+const stream = T.stream("user");
 
-stream.on('follow', function(eventMsg){
-    console.log('Followed! ...');
+stream.on("follow", eventMsg => {
+  console.log("Followed! ...");
 
-    let name = eventMsg.source.name;
-    let screenName = eventMsg.source.screen_name;
+  let name = eventMsg.source.name;
+  let screenName = eventMsg.source.screen_name;
 
-    tweetThis('@'+screenName+' Hi '+screenName+', thanks for following me. The internet needs us! We are fighting for Net Neutrality.\n' + 
-    'Go to https://www.battleforthenet.com to learn more.\n\n' +
-    '//This is a bot created to help raise awareness for Net Neutrality.')
+  tweetThis(
+    "@" +
+      screenName +
+      " Hi " +
+      screenName +
+      ", thanks for following me. The internet needs us! We are fighting for Net Neutrality.\n" +
+      "Go to https://www.battleforthenet.com to learn more.\n\n" +
+      "//This is a bot created to help raise awareness for Net Neutrality."
+  );
 });
 
-function tweetThis(text){
-    const tweet = {
-        status: text
-    }
+function tweetThis(text) {
+  const tweet = {
+    status: text
+  };
 
-    T.post('statuses/update', tweet, tweeted);
+  T.post("statuses/update", tweet, tweeted);
 
-    function tweeted(err, data, response){
-        if(err){
-            console.log("Error Ocurred!");
-        }else{
-            console.log('It worked');
-        }
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("Error Ocurred!");
+    } else {
+      console.log("It worked");
     }
+  }
 }
-
-
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 //You can find this information right after you create your twitter application
 
-module.exports = {    
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
-}
+module.exports = {
+  consumer_key: "",
+  consumer_secret: "",
+  access_token: "",
+  access_token_secret: ""
+};
